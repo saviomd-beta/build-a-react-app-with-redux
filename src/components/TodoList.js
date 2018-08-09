87ba1e4258e5f8050dfc6db32d13a9c0637db085
Sávio Mendes <saviomd@gmail.com>
2018-08-09 16:28:29 -0300

    Lesson 23

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,9 +1,12 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
-import {fetchTodos, toggleTodo} from '../reducers/todo';
+import {fetchTodos, toggleTodo, deleteTodo} from '../reducers/todo';
 
-const TodoItem = ({id, name, isComplete, toggleTodo}) => (
+const TodoItem = ({id, name, isComplete, toggleTodo, deleteTodo}) => (
   <li>
+    <span className='delete-item'>
+      <button onClick={()=> deleteTodo(id)}>X</button>
+    </span>
     <input
       type="checkbox"
       checked={isComplete}
@@ -26,6 +29,7 @@ class TodoList extends Component {
             <TodoItem
               key={todo.id}
               toggleTodo={this.props.toggleTodo}
+              deleteTodo={this.props.deleteTodo}
               {...todo}
             />
           )}
@@ -37,5 +41,5 @@ class TodoList extends Component {
 
 export default connect(
   (state) => ({todos: state.todo.todos}),
-  {fetchTodos, toggleTodo}
+  {fetchTodos, toggleTodo, deleteTodo}
 )(TodoList);
